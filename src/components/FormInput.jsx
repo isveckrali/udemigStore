905206d489c2c2c694efef5cdaac7b3b179cf18f
isveckrali <mehmet.can.seyhan1@gmail.com>
2024-10-14 21:42:41 +0200

    login added

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const FormInput = () => {
-  return (
-    <div>
-      Form Input
-    </div>
-  )
+    return (
+        <div className="form-control">
+            <label className="label">
+                <span className="label-text">What is your name?</span>
+            </label>
+            <input type="text" placeholder="Type here" className="input input-bordered" />
+        </div>
+    )
 }
 
 export default FormInput
