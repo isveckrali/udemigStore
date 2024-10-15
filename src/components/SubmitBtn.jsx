fb149b440e49b1aa43f8db200f99e99d95a968c7
isveckrali <mehmet.can.seyhan1@gmail.com>
2024-10-15 11:32:47 +0200

    submit btn added

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { useNavigation } from 'react-router-dom';
 
-const SubmitBtn = () => {
+const SubmitBtn = ({ text }) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
-    <div>
-      Submit btn
-    </div>
-  )
-}
-
-export default SubmitBtn
+    <button
+      type='submit'
+      className='btn btn-primary btn-block'
+      disabled={isSubmitting}
+    >
+      {isSubmitting ? (
+        <>
+          <span className='loading loading-spinner'></span>
+          sending...
+        </>
+      ) : (
+        text || 'submit'
+      )}
+    </button>
+  );
+};
+export default SubmitBtn;
