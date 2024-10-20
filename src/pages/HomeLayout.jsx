9867061b530a1ce137fab4a1877702fdf58fd922
isveckrali <mehmet.can.seyhan1@gmail.com>
2024-10-20 21:53:14 +0200

    header added

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
+import { Header, Navbar } from "../components";
 
 const HomeLayout = () => {
   return (
     <>
-      <nav>
-        <span className='text-4xl text-primary'>Udemig</span>
-      </nav>
+      <Header />
+      <Navbar />
       <section className='align-element py-20 '>
         <Outlet />
-      </section>
+      </section>  
     </>
   )
 }
